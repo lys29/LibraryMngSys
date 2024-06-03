7f42bf77fd1c8de5ee813c527a56f3b7116ca9ff
Dana Ellysa <59102448+lys29@users.noreply.github.com>
2024-06-04 00:23:48 +0800

    route

---
+++ library_management_system/src/app/pages/Homepage.tsx
@@ -0,0 +1,10 @@
+// 'use client'
+import React from 'react';
+
+export default function Homepage() {
+    return(
+        <div>
+            <h1>yooo</h1>
+        </div>
+    )
+}
---
+++ library_management_system/src/app/pages/Login.tsx
@@ -0,0 +1,10 @@
+// 'use client'
+import React from 'react';
+
+export default function Login() {
+    return(
+        <div>
+            <h1>loggg here</h1>
+        </div>
+    )
+}
